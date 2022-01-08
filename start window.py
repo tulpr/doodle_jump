bfe639e9ef09f7d64f58ed90342d8c5710e5452f
Tulescha <anisina2005@gmail.com>
2022-01-08 15:32:47 +0500

    пауза

--- start window.py
+++ start window.py
@@ -99,61 +99,69 @@ def play_screen():
     y_pr = y0
     platforms = [[0, 130], [-100, -50]]
     moras = [0, 0]
+    pause = 0
 
     while 1:
-        col = []
-        t += 1 / FPS
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 sys.exit()
-        # сгенерируем список платформ длины n_plat
-        if len(platforms) <= n_plat:
-            platforms.append([random.randrange(-WIDTH // 2, WIDTH // 2, 40),
-                              random.randrange(-int(1.1 * HEIGHT) // 2, -HEIGHT // 2, 60)])
-            moras.append(random.randint(0, 1))
-        y = y0 - v * t + (a * t ** 2) / 2
-        cc = (y - y_pr) * FPS
-        y_pr = y
-        screen.blit(fon, (0, 0))
-        # pygame.draw.circle(screen, BLUE, (x, y), r)
-        hero = draw_hero(x, y, screen)
-        for indx, pl in enumerate(platforms):
-            mora_col = 0
-            platform = draw_hero(pl[0], pl[1], screen, name='platform')
-            if moras[indx]:
-                mora = draw_hero(pl[0] + 60, pl[1] - 40, screen, name='mora')
-                mora_col = pygame.sprite.collide_rect(hero, mora)
-            col = pygame.sprite.collide_rect(hero, platform)
-            if col:
-                y0 = y
-                t = 0
-                v = v0
-            if mora_col:
-                count_mora += 10
-                moras[indx] = 0
-        # отрисовка текста
-        textsurface = myfont.render(f'Мора: {count_mora}', False, (0, 0, 0))
-        # сдвигаем платформы вниз
-        for indx, _ in enumerate(platforms):
-            platforms[indx][1] += 1
-        # удаление платформ вышедшие за край экрана
-        for indx, _ in enumerate(platforms):
-            if platforms[indx][1] > 100:
-                del platforms[indx]
-                del moras[indx]
-        if y > 100:
-            image = pygame.image.load('data\\end.jpg')
-            fon = pygame.transform.scale(image, (WIDTH, HEIGHT))
+            elif i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_p:
+                    if pause:
+                        pause = 0
+                    else:
+                        pause = 1
+        if pause == 0:
+            col = []
+            t += 1 / FPS
+            # сгенерируем список платформ длины n_plat
+            if len(platforms) <= n_plat:
+                platforms.append([random.randrange(-WIDTH // 2, WIDTH // 2, 40),
+                                  random.randrange(-int(1.1 * HEIGHT) // 2, -HEIGHT // 2, 60)])
+                moras.append(random.randint(0, 1))
+            y = y0 - v * t + (a * t ** 2) / 2
+            cc = (y - y_pr) * FPS
+            y_pr = y
             screen.blit(fon, (0, 0))
-        screen.blit(textsurface, (200, 0))
-        pygame.display.update()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            x -= 3
-        elif keys[pygame.K_RIGHT]:
-            x += 3
+            # pygame.draw.circle(screen, BLUE, (x, y), r)
+            hero = draw_hero(x, y, screen)
+            for indx, pl in enumerate(platforms):
+                mora_col = 0
+                platform = draw_hero(pl[0], pl[1], screen, name='platform')
+                if moras[indx]:
+                    mora = draw_hero(pl[0] + 60, pl[1] - 40, screen, name='mora')
+                    mora_col = pygame.sprite.collide_rect(hero, mora)
+                col = pygame.sprite.collide_rect(hero, platform)
+                if col:
+                    y0 = y
+                    t = 0
+                    v = v0
+                if mora_col:
+                    count_mora += 10
+                    moras[indx] = 0
+            # отрисовка текста
+            textsurface = myfont.render(f'Мора: {count_mora}', False, (0, 0, 0))
+            # сдвигаем платформы вниз
+            for indx, _ in enumerate(platforms):
+                platforms[indx][1] += 1
+            # удаление платформ вышедшие за край экрана
+            for indx, _ in enumerate(platforms):
+                if platforms[indx][1] > 100:
+                    del platforms[indx]
+                    del moras[indx]
+            if y > 100:
+                image = pygame.image.load('data\\end.jpg')
+                fon = pygame.transform.scale(image, (WIDTH, HEIGHT))
+                screen.blit(fon, (0, 0))
+            screen.blit(textsurface, (200, 0))
+            pygame.display.update()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                x -= 3
+            elif keys[pygame.K_RIGHT]:
+                x += 3
 
-        clock.tick(FPS)
+            clock.tick(FPS)
 
 
 start_screen()
