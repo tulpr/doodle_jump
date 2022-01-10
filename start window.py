c6dbdfe6795a6866baadc049afa7334e7d78a994
Tulescha <anisina2005@gmail.com>
2022-01-10 20:27:51 +0500

    обавлена музыка и монстры

--- start window.py
+++ start window.py
@@ -3,11 +3,13 @@ import sys
 
 import pygame
 
-from functions import load_image, mora_in_db, mora_from_db, get_slime_name, get_skin_id, get_skins_bool, new_skin,\
+from functions import load_image, mora_in_db, mora_from_db, get_slime_name, get_skin_id, get_skins_bool, new_skin, \
     change_skin
 
 pygame.font.init()
 pygame.init()
+pygame.mixer.music.load('data\\Genshin music.mp3')
+pygame.mixer.music.play()
 size = WIDTH, HEIGHT = 500, 600
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 screen = pygame.display.set_mode(size)
@@ -166,6 +168,8 @@ def play_screen():
             hero_img = pygame.transform.scale(hero_img, (70, 120))
         if name == 'mora':
             hero_img = pygame.transform.scale(hero_img, (30, 30))
+        if name == 'monster':
+            hero_img = pygame.transform.scale(hero_img, (100, 100))
         hero = pygame.sprite.Sprite(all_sprites)
 
         hero.image = hero_img
@@ -190,9 +194,13 @@ def play_screen():
     y_pr = y0
     platforms = [[0, 130], [-100, -50]]
     moras = [0, 0]
+    monsters = [0, 0]
     pause = 0
-
+    mus_pause = 0
+    volume = 0.5
+    fl = 0
     while 1:
+        fl -= 1
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 mora_in_db(count_mora)
@@ -203,6 +211,28 @@ def play_screen():
                         pause = 0
                     else:
                         pause = 1
+                elif i.key == pygame.K_SPACE:
+                    if mus_pause:
+                        mus_pause = 0
+                    else:
+                        mus_pause = 1
+                    # pygame.mixer.music.stop()
+                elif i.key == pygame.K_o:
+                    pygame.mixer.music.unpause()
+                    # pygame.mixer.music.play()
+                    volume += 0.1
+                elif i.key == pygame.K_j:
+                    pygame.mixer.music.unpause()
+                    volume -= 0.1
+                    # pygame.mixer.music.play()
+        pygame.mixer.music.set_volume(volume)
+        # print(pygame.mixer.music.get_volume())
+        # print(volume)
+        pygame.time.delay(20)
+        if mus_pause:
+            pygame.mixer.music.pause()
+        else:
+            pygame.mixer.music.unpause()
         if pause == 0:
             col = []
             t += 1 / FPS
@@ -211,6 +241,7 @@ def play_screen():
                 platforms.append([random.randrange(-WIDTH // 2, WIDTH // 2, 40),
                                   random.randrange(-int(1.1 * HEIGHT) // 2, -HEIGHT // 2, 60)])
                 moras.append(random.randint(0, 1))
+                monsters.append(0) if moras[-1] == 1 else monsters.append(random.randint(1, 1))
             y = y0 - v * t + (a * (t ** 2)) / 2
             cc = (y - y_pr) * FPS
             y_pr = y
@@ -219,6 +250,7 @@ def play_screen():
             hero = draw_hero(x, y, screen, get_slime_name())
             for indx, pl in enumerate(platforms):
                 mora_col = 0
+                monster_col = 0
                 platform = draw_hero(pl[0], pl[1], screen, name='platform')
                 if moras[indx]:
                     mora = draw_hero(pl[0] + 60, pl[1] - 40, screen, name='mora')
@@ -229,41 +261,51 @@ def play_screen():
                     t = 0
                     v = v0
                 if mora_col:
-                    count_mora += 1000
+                    count_mora += 10
                     moras[indx] = 0
-            # отрисовка текста
-            textsurface = myfont.render(f'Мора: {count_mora}', False, (254, 246, 238))
-            # сдвигаем платформы вниз
-            for indx, _ in enumerate(platforms):
-                platforms[indx][1] += 1
-            # удаление платформ вышедшие за край экрана
-            for indx, _ in enumerate(platforms):
-                if platforms[indx][1] > 100:
-                    del platforms[indx]
-                    del moras[indx]
-            if y > 100:
-                image = pygame.image.load('data\\end.jpg')
-                fon = pygame.transform.scale(image, (WIDTH, HEIGHT))
-                screen.blit(fon, (0, 0))
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        mora_in_db(count_mora)
-                        terminate()
-                    elif event.type == pygame.KEYDOWN:
-                        mora_in_db(count_mora)
-                        if event.key == pygame.K_n:
-                            return
-                        if event.key == pygame.K_x:
-                            terminate()
-            screen.blit(textsurface, (200, 0))
-            pygame.display.update()
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                x -= 3
-            elif keys[pygame.K_RIGHT]:
-                x += 3
+                if monsters[indx]:
+                    monster = draw_hero(pl[0] + 60, pl[1] - 40, screen, name='monster')
+                    monster_col = pygame.sprite.collide_rect(hero, monster)
 
-            clock.tick(FPS)
+                if monster_col and fl <= 0:
+                    if count_mora - 40 > 0:
+                        count_mora -= 40
+                    else:
+                        count_mora = 0
+                    fl = 100
+        # отрисовка текста
+        textsurface = myfont.render(f'Мора: {count_mora}', False, (254, 246, 238))
+        # сдвигаем платформы вниз
+        for indx, _ in enumerate(platforms):
+            platforms[indx][1] += 1
+        # удаление платформ вышедшие за край экрана
+        for indx, _ in enumerate(platforms):
+            if platforms[indx][1] > 100:
+                del platforms[indx]
+                del moras[indx]
+                del monsters[indx]
+        if y > 100:
+            image = pygame.image.load('data\\end.jpg')
+            fon = pygame.transform.scale(image, (WIDTH, HEIGHT))
+            screen.blit(fon, (0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mora_in_db(count_mora)
+                    terminate()
+                elif event.type == pygame.KEYDOWN:
+                    mora_in_db(count_mora)
+                    if event.key == pygame.K_n:
+                        return
+                    if event.key == pygame.K_x:
+                        terminate()
+        screen.blit(textsurface, (200, 0))
+        pygame.display.update()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x -= 3
+        elif keys[pygame.K_RIGHT]:
+            x += 3
+        clock.tick(FPS)
 
 
 start_screen()
