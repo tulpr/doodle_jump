dc8bb992898053d7b420115606786b65c8748b50
basova.ea61 <basova.lisa@mail.ru>
2022-01-11 16:59:22 +0500

    изменена структура программы

---
+++ const.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.font.init()
+pygame.init()
+pygame.mixer.music.load('data\\Genshin music.mp3')
+pygame.mixer.music.play()
+size = WIDTH, HEIGHT = 500, 600
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Doodle jump')
---
+++ rules_screen.py
@@ -0,0 +1,16 @@
+from const import *
+from terminate import *
+
+
+def rules_screen():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    return
+        image = pygame.image.load('data\\rules.jpg')
+        fon = pygame.transform.scale(image, (WIDTH, HEIGHT))
+        screen.blit(fon, (0, 0))
+        pygame.display.flip()
